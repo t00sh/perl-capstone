1d4a3924fa6bb26aee30f6d9a210e12a1aa596c7
-TOSH- <tosh@t0x0sh.org>
2015-07-19 15:57:16 +0200

    Improve build_releases.sh script

--- scripts/build_releases.sh
+++ scripts/build_releases.sh
@@ -1,42 +1,49 @@
 #!/bin/sh
 
-set -e
-
 if test -z $1
 then
     echo "Usage : $0 <version> (e.g. \"1.3\")"
     exit 1
 fi
 
-echo "[+] Genere readme's"
-scripts/gen_readmes.sh
-git add README*
-git commit -m "Regenere README's" || expr 1
-git push
+function cmd_to_devnull() {
+    $* > /dev/null 2>/dev/null
+    if test $? -ne 0
+    then
+        echo "[-] Command \"$*\" failed"
+        exit 1
+    fi
+}
 
 echo "[+] Adding GIT tag v$1"
-git tag v$1
-git push origin v$1
+cmd_to_devnull git tag v$1
+cmd_to_devnull git push origin v$1
+
 
 echo ""
 echo ""
 echo "~~~~~~~~~~~~ CPAN ~~~~~~~~~~~~~~"
 echo "[+] Genere CPAN archive"
-scripts/gen_cpan_zip.sh
+cmd_to_devnull scripts/gen_cpan_zip.sh
 echo "[+] You can upload pkgs/cpan/Capstone.zip to : https://pause.perl.org/pause/authenquery?ACTION=add_uri"
 
 echo ""
 echo ""
 echo "~~~~~~~~~~ ArchLinux ~~~~~~~~~~~"
 echo "[+] Genere archlinux package"
+
 cd pkgs/archlinux
 sed -i -r "s/pkgver=.+/pkgver=$1/g" PKGBUILD
-SHA_SUMS=$(makepkg -g)
+
+SHA_SUMS=$(makepkg -g 2>/dev/null)
 sed -i -r "s/sha256sums=.+/$SHA_SUMS/g" PKGBUILD
-mksrcinfo
-git add PKGBUILD .SRCINFO
-git commit -m "Release v$1"
-git push
-makepkg --source
+
+cmd_to_devnull mksrcinfo
+cmd_to_devnull git add PKGBUILD .SRCINFO
+cmd_to_devnull git commit -m v$1
+cmd_to_devnull git push
+cmd_to_devnull makepkg --source
+
 echo "[+] You can upload pkg/archlinux/*.src.tar.gz to : https://aur.archlinux.org/submit/"
 
+echo "[+] Capstone v$1 released !"
