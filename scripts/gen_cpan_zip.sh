6bae46fe69562ccbc2d55fd428bb09e48c0d2764
-TOSH- <tosh@t0x0sh.org>
2016-09-14 19:53:12 +0200

    Use make dist for CPAN archives instead of ugly things

--- scripts/gen_cpan_zip.sh
+++ scripts/gen_cpan_zip.sh
@@ -1,22 +1,4 @@
 #!/bin/sh
 
-
-if test -z $1
-then
-    echo "Usage : $0 <version>"
-    exit 1
-fi
-
-CPAN_PATH="pkgs/cpan"
-ZIP_ARCHIVE="Capstone-$1.zip"
-
-rm -rf $CPAN_PATH/"Capstone"
-
-mkdir $CPAN_PATH/"Capstone"               || exit 1
-cp $(cat MANIFEST) $CPAN_PATH/"Capstone"  || exit 1
-cd $CPAN_PATH
-zip -r $ZIP_ARCHIVE "Capstone"            || exit 1
-
-rm -rf "Capstone"
-
-echo "DONE"
+perl Makefile.PL
+make dist
